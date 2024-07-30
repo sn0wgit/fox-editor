4ba7f438cf76c6496cd1d3a89a66658db971a96c
yaroslav.vyazmitin <yaroslav.vyazmitin@gmail.com>
2024-07-30 21:51:22 +0000

    all these relative paths had much simpler ways to fix...

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ window.onload = (event) => {
   localStorage.setItem("katex-theme", currentThemeName);
   console.log(currentThemeName);
   const currentTheme = document.getElementById("theme-link");
-  currentTheme.setAttribute("href", `/katex-preview-generator/${currentThemeName}.css`);
+  currentTheme.setAttribute("href", `./${currentThemeName}.css`);
   const textarea = document.getElementById("katex-code");
   const output = document.getElementById("output");
   textarea.focus();
   themebutton.addEventListener("click", () => {
     currentThemeName = themes[(themes.indexOf(currentThemeName) + 1) % themes.length];
     localStorage.setItem("katex-theme", currentThemeName);
-    currentTheme.setAttribute("href", `/katex-preview-generator/${currentThemeName}.css`);
+    currentTheme.setAttribute("href", `./${currentThemeName}.css`);
   });
   textarea.addEventListener("input", () => {
     output.innerHTML = katex.renderToString(textarea.value);
