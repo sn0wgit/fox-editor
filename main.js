cc06e9f24788b104a53b2b49b049ffbc003a1418
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2024-10-30 17:05:08 +0200

    Update main.js
    added minimal input on startup

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 window.onload = (event) => {
+  const textarea = document.getElementById("katex-code");
+  textarea.focus();
+  textarea.value = $0.value=`\\displaystyle
+
+e=\\sum^\\infin_{n=0}\\cfrac1{n!}\\Leftrightarrow\\Big(1+\\cfrac1x\\Big)^x`;
+  const output = document.getElementById("output");
+  output.innerHTML = katex.renderToString(textarea.value);
+  textarea.addEventListener("input", () => {
+    output.innerHTML = katex.renderToString(textarea.value);
+  })
+  
   const themes = ["md3light", "md3dark", "mint", "primer"];
   const themebutton = document.getElementById("theme");
   let currentThemeName = (localStorage.getItem("katex-theme") !== "null") ? localStorage.getItem("katex-theme") : themes[0];
@@ -6,18 +17,11 @@ window.onload = (event) => {
   console.log(currentThemeName);
   const currentTheme = document.getElementById("theme-link");
   currentTheme.setAttribute("href", `./${currentThemeName}.css`);
-  
-  const textarea = document.getElementById("katex-code");
-  const output = document.getElementById("output");
-  textarea.focus();
   themebutton.addEventListener("click", () => {
     currentThemeName = themes[(themes.indexOf(currentThemeName) + 1) % themes.length];
     localStorage.setItem("katex-theme", currentThemeName);
     currentTheme.setAttribute("href", `./${currentThemeName}.css`);
   });
-  textarea.addEventListener("input", () => {
-    output.innerHTML = katex.renderToString(textarea.value);
-  })
 
   const fullscreenButton = document.getElementById("fullscreen");
   fullscreenButton.addEventListener("click", () => {
