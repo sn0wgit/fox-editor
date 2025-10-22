8a1b9d82d63cab2cc96ae512026d01dc2e81b2aa
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2025-10-22 14:34:21 +0300

    no way i added text file import

--- iframe/main.js
+++ iframe/main.js
@@ -1,14 +1,42 @@
-window.onload = (event) => {
+async function processSrcFile(srcFile){
+    try {
+        const response = await fetch(srcFile, {method: 'GET', headers: {'Accept': 'text/plain'}}).then(response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка сети или сервера, статус: ${response.status}`);
+            }
+            return response.text(); 
+        }).then(textData => {render(textData)}).catch(error => {
+            console.error('oops:', error);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+function render(appendedText){
     const url = new URL(window.location.href);
-    const code = url.searchParams.get("q");
+    const query = url.searchParams.get("q");
+    const srcFile = url.searchParams.get("src");
+    if (srcFile != "null" && appendedText == ""){
+        processSrcFile(srcFile)
+    }
+    console.log(query);
+    const code = (query == null) ? appendedText : query + appendedText;
+    console.log(code);
     const color = url.searchParams.get("color");
     const background = url.searchParams.get("background");
     const output = document.getElementById("output");
-    output.innerHTML = katex.renderToString(code);
+    output.innerHTML = katex.renderToString(code, {
+        output: "html",
+        trust: true,
+    });
     if (color != "null"){
         output.style.color = `#${color}`;
     }
     if (background != "null"){
         output.style.backgroundColor = `#${background}`;
     }
+}
+
+window.onload = (event) => {
+    render("")
 }
